d650f74b4723efbe942c265ae986cda55915999f
joanpujol <joangalilea@gmail.com>
2019-09-30 20:58:03 +0200

    Remove unnecessary elements from project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import './styles/global.css';
 
 import withContext from "./Context";
@@ -11,25 +9,7 @@ function App() {
   const HeaderWithContext = withContext(Header);
 
   return (
-    <div className="App">
-
     <HeaderWithContext />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
