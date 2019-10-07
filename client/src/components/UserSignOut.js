cd712a4d046f74a2954f9ec17665914e9b416a01
joanpujol <joangalilea@gmail.com>
2019-10-07 12:32:18 +0200

    Add UserSignOut functional component

---
+++ client/src/components/UserSignOut.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const UserSignOut = () => {
+    return(
+        <Redirect to="/" />
+    );
+}
+
+export default UserSignOut;
