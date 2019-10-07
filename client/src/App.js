e78479eab80aa002ec4117661b5aeb12b2011f80
joanpujol <joangalilea@gmail.com>
2019-10-07 12:49:00 +0200

    Add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './styles/global.css';
 
 import withContext from "./Context";
@@ -8,22 +9,26 @@ import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp'
 import CourseDetail from './components/CourseDetail';
 import CreateCourse from './components/CreateCourse';
-import UpdateCourse from './components/UpdateCourse'
+import UpdateCourse from './components/UpdateCourse';
+import UserSignOut from './components/UserSignOut';
 
 function App() {
 
   const HeaderWithContext = withContext(Header);
 
   return (
-    <>
+    <BrowserRouter>
       <HeaderWithContext />
-      <Courses />
-      <CourseDetail />
-      <UserSignIn />
-      <UserSignUp />
-      <CreateCourse />
-      <UpdateCourse />
-    </>
+      <Switch>
+        <Route exact path="/" component={Courses}/>
+        <Route path="/courses/create" component={CreateCourse} />
+        <Route path="/courses/:id" component={CourseDetail} />
+        <Route path="/courses/:id/update" component={UpdateCourse} />
+        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOut} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
